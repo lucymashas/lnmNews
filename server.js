0fc2027a3ce4e810e262493b905badd67972ab99
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-03-18 19:42:56 -0400

    Heroku

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
-require("./routes/viewroute.js")(app);
+require("./routes/")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
