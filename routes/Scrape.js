da3c5b67a3628ef52799ef87a65c34f9fe14950a
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-03-22 20:25:35 -0400

    update to heroku

--- routes/Scrape.js
+++ routes/Scrape.js
@@ -9,7 +9,19 @@ var ObjectId = require('mongodb').ObjectId;
 
 mongoose.Promise = Promise;
 
-mongoose.connect("mongodb://localhost/lnmNews");
+// mongoose.connect("mongodb://localhost/lnmNews");
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/lnmNews";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
 
 //Require db model
 var db = require("../models");
